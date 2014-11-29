443f0733c0a06a3d5bc20334e17d45283a386031
Georg Konwisser <gekonwi@gmail.com>
2014-11-29 15:37:34 -0500

    combined files from PS4 and PS5 into the new 'parser' directory since PS5 builds on the parser of PS4

---
+++ parser/World.hs
@@ -0,0 +1,30 @@
+module World where
+
+import HRAS
+
+type Prop = String
+
+data ModalOperator = Necessarily | Possibly deriving (Show, Eq)
+
+data TemporalOperator = H | P | F | G deriving (Show, Eq)
+
+data TProp = TProp { tempOp :: TemporalOperator,
+     	     	     prop ::  Prop } deriving (Show, Eq)
+
+data World = World { propositions :: [Prop] } deriving (Show, Eq)
+
+w1 = World { propositions = [] }
+w2 = World { propositions = [] }
+w3 = World { propositions = [] }
+w4 = World { propositions = [] }
+w5 = World { propositions = [] }
+
+model = [w1,w2,w3,w4,w5]
+
+-- isValid :: TProp -> Bool
+
+-- isSatisfiable :: TProp -> Bool
+
+-- isSatisfied :: TProp -> World -> Bool
+
+-- entailments :: Prop -> World -> [(World, Prop)]
