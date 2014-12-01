d7d0a9140bfe03ece39add7f0304af4564c04f36
Georg Konwisser <gekonwi@gmail.com>
2014-11-30 20:23:12 -0500

    #19: added number :: Lexicon.Feat argument to Prop.conjugateVerb
    created unit tests for all 24 cases
    adapted the unit testing framework I developed for PS3
    
    Closes #19.

---
+++ parser/UnitTest.hs
@@ -0,0 +1,57 @@
+module UnitTest where
+
+indent :: (Show a) => a -> String
+indent x = "    " ++ ( filter (\c -> c /= '"') $ show x)
+
+
+sepLine :: String -> String
+sepLine c = (concat $ take 60 $ repeat c)
+
+runTests :: (Show a, Eq a) => [(String, a, a)] -> ([String], Int)
+runTests tests = (concat details, errorCount)
+	where
+		details = [ oneTestDetails | (name, actual, expected) <- tests,
+			let header h1 h2 = [sepLine "-", h1, indent name, h2],
+			let passedHead = header "Test Passed:" "expected == actual:",
+			let failedHead = (header ">>>>>>> TEST FAILED:" "expected:") 
+				++ [indent expected] ++ ["actual:"],
+			let detailsHead = if actual == expected then passedHead else failedHead,
+			let oneTestDetails = detailsHead ++ [indent actual]
+			]
+
+		errorCount = sum [boolNum | (_, actual, expected) <- tests,
+			let boolNum = if actual == expected then 0 else 1
+			]
+
+
+showErrorCount :: Int -> String
+showErrorCount count = 
+	if count == 0 
+	then "All tests passed :D"
+	else ">>>>>>> FAILED TESTS: " ++ show count
+
+
+showTestResults :: [([String], Int)] -> [String]
+showTestResults results = (concat lineGroups) ++ lastLines
+	where 
+		lineGroups = [ header ++ details ++ errorCount | r <- results,
+			let header = ["", "", "Starting test suite:"],
+			let details = map indent $ fst r,
+			let errorCount = [(sepLine "="), showErrorCount $ snd r]
+			]
+
+		lastLines = 
+			["", "", "", sepLine "=", "Result of all test suites:"]
+			++ [showErrorCount $ sum [ snd r | r <- results]]
+			++ [sepLine "=", "", ""]
+
+
+runUnitTests :: (Show a, Eq a) => [(String, a, a)] -> IO()
+runUnitTests testSuite = do
+	-- can't put all test suites at once since they have different types
+	mapM_ putStrLn $ showTestResults $ [
+		--runTests whereTests,
+		--runTests whoTests,
+		--runTests whatTests,
+		runTests testSuite
+		]
