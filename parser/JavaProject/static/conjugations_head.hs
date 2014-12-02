680583671982acc963d188d50884ffc887e73c27
Georg Konwisser <gekonwi@gmail.com>
2014-12-02 05:54:31 -0500

    regenerated VerbRoot.hs and Conjugations.hs using the added BVL rules this should have added some missing entries

--- parser/JavaProject/static/conjugations_head.hs
+++ parser/JavaProject/static/conjugations_head.hs
@@ -11,7 +11,3 @@ module Conjugations where
 -- sample output: ["ate", "have_eaten", "has_eaten", "eat", "eats", "will_eat"]
 conjugations :: String -> [String]
 
--- these two verbs are used in unit tests but not given in the Brandeis Verb Lexicon, thus static here
-conjugations "smile" = ["smiled", "have_smiled", "has_smiled", "smile", "smiles", "will_smile"]
-conjugations "shout" = ["shouted", "have_shouted", "has_shouted", "shout", "shouts", "will_shout"]
-
--- parser/JavaProject/static/root_head.hs
+++ parser/JavaProject/static/root_head.hs
@@ -4,19 +4,3 @@ module VerbRoot where
 -- e.g. root "ate" or root "have_eaten" both result in the output "eat"
 root :: String -> String
 
--- these two verbs are used in unit tests but not given in the Brandeis Verb Lexicon, thus static here
-
-root "smiled" = "smile"
-root "have_smiled" = "smile"
-root "has_smiled" = "smile"
-root "smile" = "smile"
-root "smiles" = "smile"
-root "will_smile" = "smile"
-
-root "shouted" = "shout"
-root "have_shouted" = "shout"
-root "has_shouted" = "shout"
-root "shout" = "shout"
-root "shouts" = "shout"
-root "will_shout" = "shout"
-
